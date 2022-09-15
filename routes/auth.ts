e583d8751ccd82833eb8682fda029de39e5278a6
KEENTO <handball0321@gmail.com>
2022-09-15 16:01:20 -0700

    Add views and models based on MVC

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import bcrypt from "bcrypt";
+// import users from "../models/users.json";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Authentication");
+const hashPassword = async (passwordText: string, saltRound: number) => {
+  const salt = await bcrypt.genSalt(saltRound);
+  const hashedPassword = await bcrypt.hash(passwordText, salt);
+  return hashedPassword;
+};
+router.get("/login", (req: Request, res: Response) => {
+  res.render("login");
+});
+
+router.post("/login", async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = await hashPassword(password, 12);
+  console.log(hashedPassword);
+
+  res.redirect("/");
+});
+
+router.get("/signup", (req: Request, res: Response) => {
+  res.render("signup");
 });
 
 export default router;
